d815ffb88f1cd9b5767b6d5ac931681d36c9c955
Perfecto II Cayabyab <132222919+portfolioio@users.noreply.github.com>
2023-05-07 22:08:52 +0800

    1

---
+++ wp-content/mu-plugins/local-by-flywheel-live-link-helper.php
@@ -0,0 +1,6 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function get_option() in /Users/perfectocayabyab/Local Sites/gorider/app/public/wp-content/mu-plugins/local-by-flywheel-live-link-helper.php:21
+Stack trace:
+#0 /Users/perfectocayabyab/Local Sites/gorider/app/public/wp-content/mu-plugins/local-by-flywheel-live-link-helper.php(216): LocalWP_Live_Link_Helper-&gt;__construct()
+#1 {main}
+  thrown in <b>/Users/perfectocayabyab/Local Sites/gorider/app/public/wp-content/mu-plugins/local-by-flywheel-live-link-helper.php</b> on line <b>21</b><br>
